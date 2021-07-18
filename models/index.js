dd4fded609a212aea96dcd7843a68dbb0bc61763
dmobrien <dmobrienllc@gmail.com>
2021-07-18 12:10:48 -0500

    Sequelize configured, models have columns, associations created, data seeded.

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  foreignKey: 'product_id',
+  through: ProductTag,
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foreignKey: 'tag_id',
+  through: ProductTag,
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
